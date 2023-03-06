0d8b8f40db0f477aaf7741b91391fc41630b37ae
paisalll <faizaltriasaa@gmail.com>
2023-03-06 09:39:15 +0700

    Slice Component

---
+++ src/page/user/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Layout from "../../components/Layout";
+
+
+const UserList = () => {
+    return (
+        <Layout> 
+            <div className="drawer drawer-mobile">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                </div> 
+                <div className="drawer-side bg-slate-500">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
+                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <li><a>Sidebar Item 1</a></li>
+                    <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default UserList
