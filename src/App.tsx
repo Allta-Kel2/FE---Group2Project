18d9e137d7cf2315b9c87d8fc9ee2bc507666ac0
fmqonik <kaniafajriya@gmail.com>
2023-03-06 21:56:19 +0700

    feat: slicing Login Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 
+import Login from './page/auth/Login';
 import Dashboard from './page/dashboard';
 import UserList from './page/user';
 import Mentee from './page/mentee';
@@ -16,6 +16,7 @@ function App() {
     <CookiesProvider>
       <BrowserRouter>
         <Routes>
+          <Route path='/login' element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/user" element={<UserList />} />
           <Route path="/mentee" element={<Mentee />} />
